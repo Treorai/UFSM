7306389b8725c87e6fb62414865135ddafae35c5
Treorai <50612887+Treorai@users.noreply.github.com>
2024-11-26 00:21:01 -0300

    Create Projects

---
+++ UFSM00281-2024B/ISE Projects/A02_livre/main.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:54:12 11/23/2024 
+-- Design Name: 
+-- Module Name:    main - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity main is
+    Port ( input : in  STD_LOGIC;
+           output : out  STD_LOGIC);
+end main;
+
+architecture Behavioral of main is
+
+begin
+
+
+end Behavioral;
+
