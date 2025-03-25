7282ebf8e590a55b3fcef11d15e1b72800b0fc18
Watto <50612887+Treorai@users.noreply.github.com>
2025-03-25 15:10:26 -0300

    Add files via upload

---
+++ 2025_A/UFSM00056 - Microcontroladores/Exercícios/Exercício03_25032025/Exercício03_25032025/main.asm
@@ -0,0 +1,27 @@
+;
+; Exercício03_25032025.asm
+;
+; Created: 25/03/2025 14:57:17
+; Author : watto
+;
+; Faça um programa que leia 2 valores de PORTB e PORTC e mostre a soma em PORTD.
+
+start:
+    clr r16			; r16 <- 0000 0000 | limpa o reg r16
+	clr r17			; r17 <- 0000 0000 | limpa o reg r17
+
+	out DDRB, r16	; DDRB <- 0000 0000 | dizer que PORTB é input
+	out DDRC, r16	; DDRC <- 0000 0000 | dizer que PORTC é input
+
+	ser r16			; r16 <- 1111 1111 | "SET REGISTER"
+	out DDRD, r16	; r16 <- 1111 1111 | dizer que PORTD é output
+	out PORTB, r16	; PORTB <- 1111 1111 | ligando pull up
+
+loop:
+	in r16, PINB	; PINB são os pinos de entrada de PORTB | lê a porta B
+	in r17, PINC	; PINC são os pinos de entrada de PORTC | lê a porta C
+	
+	add r16, r17	; r16 <- r16, r17
+
+	out PORTD, r16	; PORTD são os pinos pra saida
+	jmp loop
