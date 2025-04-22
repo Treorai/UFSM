cb1e179887d5a8bdf06b878915c5e5f57ad36e40
Treorai <50612887+Treorai@users.noreply.github.com>
2025-04-21 21:36:31 -0300

    Trabalho 2 Final

--- 2025_A/UFSM00056 - Microcontroladores/Projetos/7.0/Conversor hex-dec/Conversor hex-dec/main.asm
+++ 2025_A/UFSM00056 - Microcontroladores/Projetos/7.0/Conversor hex-dec/Conversor hex-dec/main.asm
@@ -4,45 +4,46 @@
 ; Created: 01-Apr-25 16:44:28
 ; Author : Watto
 ;
-; Projeto de display conversor de entrada binaria para decimal de 0 a 255
+
+.include "m328pdef.inc"
 
 start:
     clr r16
-    out DDRB, r16   ; PORTB como entrada
+    out DDRB, r16			; PORTB como entrada
 
-    ser r16         ; r16
-	out PORTB, r16	; ativa pull ups
-    out DDRD, r16   ; PORTD -> saida (unidades)
-    out DDRC, r16   ; PORTC -> saida (dezenas)
+    ser r16					; r16 = 0xFF
+	out PORTB, r16			; ativa pull ups
+    out DDRD, r16			; PORTD -> saida (unidades)
+    out DDRC, r16			; PORTC -> saida (dezenas)
 
 loop:
-    in r16, PINB    ; r16 = input
+    in r16, PINB			; r16 = input
 
-    clr r19			; r19 = centenas
-    clr r18			; r18 = dezenas
+    clr r19					; r19 = centenas
+    clr r18					; r18 = dezenas
 
-    ; calcula centenas
+; calcula centenas
 check_hundreds:
     cpi r16, 100
-    brlo skip_hundreds    ; Se <100, pula
+    brlo skip_hundreds		; Se <100, pula
     subi r16, 100
     inc r19
-    rjmp check_hundreds   ; Repete ate r16 < 100
+    rjmp check_hundreds		; Repete ate r16 < 100
 
 skip_hundreds:
     ; calcula dezenas
 check_tens:
     cpi r16, 10
-    brlo skip_tens        ; Se <10, pula
+    brlo skip_tens			; Se <10, pula
     subi r16, 10
     inc r18
-    rjmp check_tens       ; Repete até r16 < 10
+    rjmp check_tens			; Repete até r16 < 10
 
 skip_tens:
     ; agora r16 contem as unidades
     mov r17, r16
 
-	; shift r19 para os bits maiores de r17
+	; shift r19 para os bits altos de r17
 	lsl r19
 	lsl r19
 	lsl r19
@@ -52,8 +53,8 @@ skip_tens:
 
 	or r17, r19
 
-    ; print
-    out PORTD, r17		; unidades em PORTD
-    out PORTC, r18		; dezenas no PORTC
+print:
+    out PORTD, r17			; unidades em PORTD
+    out PORTC, r18			; dezenas no PORTC
 
     rjmp loop 
