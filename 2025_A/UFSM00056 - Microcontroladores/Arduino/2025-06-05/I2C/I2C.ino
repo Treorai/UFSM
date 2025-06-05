a797d17a14254de667a1ca6d0e809f1a5d86d049
Watto <50612887+Treorai@users.noreply.github.com>
2025-06-05 15:14:49 -0300

    Update I2C.ino

--- 2025_A/UFSM00056 - Microcontroladores/Arduino/2025-06-05/I2C/I2C.ino
+++ 2025_A/UFSM00056 - Microcontroladores/Arduino/2025-06-05/I2C/I2C.ino
@@ -10,6 +10,10 @@ Monitor baudrate: 115200
 */
 
 #include <Wire.h>
+char input;
+uint8_t acelx_l; // unsigned 8bits
+uint8_t acely_h;
+uint16_t acelx_final;
 
 void setup() {
   Serial.begin(9600);
@@ -19,22 +23,29 @@ void setup() {
 }
 
 void loop() {
-  // Download from gh
-
-  //
+  // Leitura Hi
   Wire.beginTransmission(0x68);
-  Wire.write(0x3C);
-  Wire.endTransmission(false);
+  Wire.write(0x3B); //ACEL XOUT HI
+  Wire.endTransmission(false); // SET BIT TO LO (READ) -- (HI = WRITE)
   Wire.requestFrom(0x68, 1);
 
   if(Wire.available()==1){
     acelx_h = Wire.read();
   }
 
-  acelx_final = (acelx_h << 16) + acelx_l;
+  // Leitura Lo
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3C); //ACEL XOUT HI
+  Wire.endTransmission(false); // SET BIT TO LO (READ) -- (HI = WRITE)
+  Wire.requestFrom(0x68, 1);
+
+  if(Wire.available()==1){
+    acelx_l = Wire.read();
+  }
+
+  acelx_final = (acelx_h << 8) + acelx_l;
   Serial.println("Acel X final ");
   Serial.print(acelx_final);
-  
 
   delay(1000);
 }
